7ea6764ca4118c6b7e1c1032adc31146fd7788d3
Salem Amba <salemamba@gmail.com>
2016-05-11 19:55:15 -0700

    added secret key

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module Sportstore
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
         origins '*'
